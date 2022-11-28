086bc80a6a13b6fbcca8f03544ccd2f63def5c72
dattatrayr <dattatrayrawool@gmail.com>
2022-11-28 21:28:47 +0530

    Add project files.

---
+++ AWSSecretsInConsoleApp/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace AWSSecretsInConsoleApp
+{
+    public class Config
+	{
+		public ConnectionDetails connectionDetails { get; set; }
+	}
+
+	public class ConnectionDetails
+	{
+		public string Database { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ AWSSecretsInConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AWSSecretsInConsoleApp;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+//pass AWS secrets to executable as environment variable
+string data = Environment.GetEnvironmentVariable("Config");
+if (!string.IsNullOrEmpty(data) && data.Trim() != "")
+{
+    MemoryStream newData = new(Encoding.Default.GetBytes(data));
+    if (newData != null) configurationBuilder.AddJsonStream(newData);
+}
+IConfigurationRoot configurationRoot = configurationBuilder.Build();
+Config? config = configurationRoot.GetSection("Config").Get<Config>();
+Console.WriteLine("Database: " + config.connectionDetails.Database);
+Console.WriteLine("Username: " + config.connectionDetails.Username);
+
+
+
